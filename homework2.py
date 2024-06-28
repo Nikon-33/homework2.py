d1efd7d74889381a9487a97c2883fd06659560d6
Nikon-33 <89101717800@mail.ru>
2024-06-28 18:19:17 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_completed_remote_sensing = 4
+Number_of_hours_spent = 1
+Course_name = "Python"
+Time_per_task = Number_of_hours_spent / Number_of_completed_remote_sensing
+print((Course_name + ","), "всего задач:", Number_of_completed_remote_sensing, "затрачено часов:", Number_of_hours_spent, "среднее время выполнения:", Time_per_task)
